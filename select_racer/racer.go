f85deb4751b298a4efab77ab1c720d27e851b146
Amit <amitc9der@gmail.com>
2025-04-25 21:21:55 +0530

    select,time.After,go routine

---
+++ select_racer/racer.go
@@ -0,0 +1,38 @@
+package select_racer
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Millisecond):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ConfigurableRacer(a, b string, delay time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(delay):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
